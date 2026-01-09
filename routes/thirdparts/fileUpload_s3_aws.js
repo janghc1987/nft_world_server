175903ffc3f34b68ac786e5a11bb5559794c822d
wkdguscjf1987@gmail.com <wkdguscjf1987@gmail.com>
2026-01-09 10:56:45 +0900

    11

--- routes/thirdparts/fileUpload_s3_aws.js
+++ routes/thirdparts/fileUpload_s3_aws.js
@@ -14,7 +14,7 @@ const s3 = new aws.S3({
 
 const storage = multerS3({
   s3: s3,
-  bucket: 'dodogonft', // s3 생성시 버킷명
+  bucket: 'world-nft-bucket', // s3 생성시 버킷명
   acl: 'public-read',   // 업로드 된 데이터를 URL로 읽을 때 설정하는 값입니다. 업로드만 한다면 필요없습니다.
   metadata: function (req, file, cb) {
     cb(null, {fieldName: file.fieldname}); // 파일 메타정보를 저장합니다.
